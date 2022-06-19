500c17e793ee249d87f5a55ef5028c788cfccee1
hajjjat2004 <53589537+hajjjat2004@users.noreply.github.com>
2022-06-19 16:41:53 +0300

    Update Format.js

--- frontend-development/epic-timesheet-and-leaves-comparison-frontend-development/src/hook/Format.js
+++ frontend-development/epic-timesheet-and-leaves-comparison-frontend-development/src/hook/Format.js
@@ -3,7 +3,7 @@ async function Format (payload,setBtn,which,setfileData) {
     const data = new FormData();
     data.append(`file`, payload.files[0]);
    
-    let form = await axios.post(`http://localhost:3010?name=${which}`, data);
+    let form = await axios.post(`http://192.168.32.134:3010?name=${which}`, data);
     let formData = form.data;
     setfileData({name:payload.files[0].name,arr:formData.sheetNames,rowL:formData.rowL})
         payload.value = null
